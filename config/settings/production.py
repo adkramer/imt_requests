d0aca5df62f55f847e49b00aa9d0cf1038b5793e
adkramer <adkramer@users.noreply.github.com>
2017-01-29 17:25:11 -0600

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -58,7 +58,7 @@ X_FRAME_OPTIONS = 'DENY'
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['imtrequests.herokuapp.com'])
 # END SITE CONFIGURATION
 
-INSTALLED_APPS += ('gunicorn', )
+INSTALLED_APPS += ('gunicorn', 'waitress' )
 
 
 # STORAGE CONFIGURATION
